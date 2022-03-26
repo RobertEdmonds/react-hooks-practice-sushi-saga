ee326f8a8418d5be9b308888ee9858d07a8e5ad0
Robert Edmonds <bobby.edmonds89@gmail.com>
2022-03-25 22:09:52 -0600

    commit tree 655d23be486316c73c0457aa5b64e3b3dee2361e

--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -2,14 +2,16 @@ import React from "react";
 import MoreButton from "./MoreButton";
 import Sushi from "./Sushi";
 
-function SushiContainer({sushi}) {
+function SushiContainer({sushi, handleChangeSushi, eatSushi}) {
+
   const displaySushi = sushi.map(item =>{
-    return <Sushi key={item.id} name={item.name} image={item.img_url} price={item.price} />
+    return <Sushi key={item.id} name={item.name} image={item.img_url} price={item.price} eatSushi={eatSushi} eaten={item.eaten} item={item}/>
   })
+
   return (
     <div className="belt">
       {displaySushi}
-      <MoreButton />
+      <MoreButton handleChangeSushi={handleChangeSushi}/>
     </div>
   );
 }
